a7c4c332c5cf5153725d2973f1ceee30e8562c09
azamwahlla786 <azamwallha@gmail.com>
2026-05-10 07:03:20 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='.')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('.', path)
+
+if __name__ == "__main__":
+    app.run()from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='.')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('.', path)
+
+if __name__ == "__main__":
+    app.run()
