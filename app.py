4d8711464a994a261262da2d0fd31122304ca30c
azamwahlla786 <azamwallha@gmail.com>
2026-05-10 08:11:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, send_from_directory
 import os
 
-# Flask app initialize
 app = Flask(__name__, static_folder='.')
 
-# Home route
 @app.route('/')
 def index():
+    # Check karein ke index.html file mojood hai
     return send_from_directory('.', 'index.html')
 
-# Dynamic route for other files
 @app.route('/<path:path>')
-def serve_static(path):
+def static_proxy(path):
+    return send_from_directory('.', path)
 
-    # Agar file exist karti hai to usay bhej do
-    if os.path.exists(path):
-        return send_from_directory('.', path)
-
-    # Agar file na mile to index page kholo
-    return send_from_directory('.', 'index.html')
-
-# Run app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
